697f619c113c028b0667c2dbc31a70dec18768d9
pontus <cpsoederlund@gmail.com>
2019-11-08 13:19:11 +0100

    add support for rarity

--- Item.java
+++ Item.java
@@ -1,16 +1,39 @@
 public class Item {
 
+    //***********************
+    // Variables
+    //***********************
     private String name;
+    private int rarity;
 
+    //***********************
+    // Constructors
+    //***********************
     public Item(String name) {
         this.name = name;
+        this.rarity = 0;
     }
 
+    public Item(String name, int rarity) {
+        this(name);
+        this.rarity = rarity;
+    }
+
+    //***********************
+    // Getters & Setters
+    //***********************
     /**
      * @return the name
      */
     public String getname() {
         return name;
+    }
+
+    /**
+     * @return the rarity
+     */
+    public int getRarity() {
+        return rarity;
     }
 
     /**
