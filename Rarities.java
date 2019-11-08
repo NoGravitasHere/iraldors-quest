64957f87b1d83a5cdbd9d16fef5de1bbd25a766e
pontus <cpsoederlund@gmail.com>
2019-11-08 15:48:43 +0100

    rename ItemOption to Items and add get random method

---
+++ Rarities.java
@@ -0,0 +1,19 @@
+import java.util.Collections;
+import java.util.*;
+
+public enum Rarities {
+    NORMAL,
+    UNCOMON,
+    RARE,
+    EPIC,
+    LEGENDARY;
+
+    private static final List<Rarities> VALUES =
+    Collections.unmodifiableList(Arrays.asList(values()));
+    private static final int SIZE = VALUES.size();
+    private static final Random RANDOM = new Random();
+
+    public static Rarities randomRarity()  {
+        return VALUES.get(RANDOM.nextInt(SIZE));
+    }
+}
--- RarityOption.java
+++
@@ -1,7 +0,0 @@
-public enum RarityOption {
-    NORMAL,
-    UNCOMON,
-    RARE,
-    EPIC,
-    LEGENDARY;
-}
