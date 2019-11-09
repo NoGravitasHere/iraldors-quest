21d37d82759e362addca765bc2fe80558f63ecb8
pontus <cpsoederlund@gmail.com>
2019-11-09 15:29:15 +0100

    add a new parser for parsing user input

---
+++ Parser.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+
+public class Parser {
+
+    private ArrayList<String> redundantWords;
+
+    public Parser() {
+        redundantWords = new ArrayList<>();
+        redundantWords.add("the");
+        redundantWords.add("at");
+        redundantWords.add("to");
+    }
+
+    public Action parse(String input) {
+        input = input.toLowerCase();
+        input = removeRedundantWords(input);
+
+        String v = getVerb(input);
+        Verbs verb = parseVerb(v);
+
+        ArrayList<Nouns> nouns = new ArrayList<>();
+        var ns = getNouns(input);
+        for (String string : ns) {
+            nouns.add(parseNoun(string));
+        }
+        return new Action(verb, nouns);
+    }
+
+    private String getVerb(String input) {
+        if(input.indexOf(" ") == -1) {
+            return input;
+        } else {
+            return input.substring(0, input.indexOf(" "));
+        }
+    }
+
+    private ArrayList<String> getNouns(String input) {
+        var list = new ArrayList<String>();
+        if(input.indexOf(" ") == -1) {
+            return list;
+        }
+        input = input.substring(getVerb(input).length() + 1, input.length());
+        input = input.replaceAll(" ", "");
+        return new ArrayList<String>(Arrays.asList(input.split(",")));
+    }
+
+    private Verbs parseVerb(String verb) {
+        switch(verb.toLowerCase()) {
+            case "help":
+            return Verbs.HELP;
+
+            case "quit":
+            return Verbs.QUIT;
+
+            case "move":
+            case "go":
+            case "m":
+            case "mv":
+            return Verbs.MOVE;
+
+            case "talk":
+            case "t":
+            return Verbs.TALK;
+
+            case "attack":
+            case "atk":
+            case "a":
+            return Verbs.ATTACK;
+
+            case "take":
+            return Verbs.TAKE;
+
+            default:
+            return Verbs.UNKNOWN;
+        }
+    }
+
+    private Nouns parseNoun(String noun) {
+        switch(noun.toLowerCase()) {
+            case "north":
+            case "w":
+            return Nouns.NORTH;
+
+            case "south":
+            case "s":
+            return Nouns.SOUTH;
+
+            case "east":
+            case "d":
+            return Nouns.EAST;
+
+            case "west":
+            case "a":
+            return Nouns.WEST;
+
+            case "items":
+            return Nouns.ITEMS;
+
+            default:
+            if(noun.matches("(item|i)(\\d+)?")) {
+                return Nouns.ITEM;
+            }
+
+            return Nouns.UNKOWN;
+        }
+    }
+
+    private String removeRedundantWords(String text) {
+        StringBuilder sb = new StringBuilder(text);
+        for (String word: redundantWords) {
+            while(0 < sb.indexOf(word)) {
+                sb.delete(sb.indexOf(word), sb.indexOf(word) + word.length());
+            }
+        }
+        return sb.toString();
+    }
+}
